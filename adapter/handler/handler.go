84a62fd14872049d182c9b0ece42fb8876b75afe
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2022-11-15 23:40:25 +0900

    feat: add graphql endpoint to handler

--- adapter/handler/handler.go
+++ adapter/handler/handler.go
@@ -3,6 +3,10 @@ package handler
 import (
 	"net/http"
 
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/Pranc1ngPegasus/middlechain"
 	"github.com/google/wire"
 )
 
@@ -14,17 +18,24 @@ var NewHandlerSet = wire.NewSet(
 )
 
 type Handler struct {
+	schema graphql.ExecutableSchema
+
 	router http.Handler
 }
 
-func NewHandler() *Handler {
+func NewHandler(
+	schema graphql.ExecutableSchema,
+) *Handler {
 	mux := http.NewServeMux()
 
 	h := &Handler{
+		schema: schema,
 		router: mux,
 	}
 
 	mux.HandleFunc("/ping", h.ping)
+	mux.HandleFunc("/graphql", h.graphql)
+	mux.HandleFunc("/play", h.playground)
 
 	return h
 }
@@ -33,6 +44,17 @@ func (h *Handler) ping(w http.ResponseWriter, r *http.Request) {
 	if _, err := w.Write([]byte("pong")); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
+}
+
+func (h *Handler) graphql(w http.ResponseWriter, r *http.Request) {
+	srv := handler.NewDefaultServer(h.schema)
+	handler := middlechain.Chain(srv)
+	handler.ServeHTTP(w, r)
+}
+
+func (h *Handler) playground(w http.ResponseWriter, r *http.Request) {
+	play := playground.Handler("Trial Field", "/graphql")
+	play.ServeHTTP(w, r)
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
