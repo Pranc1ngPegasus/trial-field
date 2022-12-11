281b88fa893bc4223b73c8d50432b5fb726f2f35
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2022-12-11 23:49:41 +0900

    fix: follow logger diff

--- adapter/server/server.go
+++ adapter/server/server.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 	"time"
 
@@ -10,13 +10,14 @@ import (
 )
 
 func NewServer(
+	ctx context.Context,
 	logger logger.Logger,
 	config configuration.Configuration,
 	rootHandler http.Handler,
 ) *http.Server {
 	cfg := config.Config()
 
-	logger.Info(fmt.Sprintf("listen on %s", cfg.ServerPort))
+	logger.Info(ctx, "listen on", logger.Field("port", cfg.ServerPort))
 
 	return &http.Server{
 		Addr:              ":" + cfg.ServerPort,
